d6f6b0dd197597b70528ff8301c5af3c38a99a1f
keidsondesigner <keidsondesigner@gmail.com>
2023-07-28 23:45:49 -0400

    ajustes assets/svg

--- lib/shared/app_images.dart
+++ lib/shared/app_images.dart
@@ -1,11 +1,11 @@
 class AppImages {
-  static String logo = "lib/images/logo.svg";
-  static String notification = "lib/images/notification.svg";
-  static String up = "lib/images/up.svg";
-  static String down = "lib/images/down.svg";
-  static String moneyCircle = "lib/images/money_circle.svg";
-  static String handMoney = "lib/images/hand_money.svg";
-  static String screwdriver = "lib/images/screwdriver.svg";
-  static String circleOn = "lib/images/circle_on.svg";
-  static String circleOff = "lib/images/circle_off.svg";
+  static String logoSantander = "lib/assets/imgs/logo_santander.svg";
+  static String notification = "lib/assets/imgs/notification.svg";
+  static String up = "lib/assets/imgs/up.svg";
+  static String down = "lib/assets/imgs/down.svg";
+  static String moneyCircle = "lib/assets/imgs/money_circle.svg";
+  static String handMoney = "lib/assets/imgs/hand_money.svg";
+  static String screwdriver = "lib/assets/imgs/screwdriver.svg";
+  static String circleOn = "lib/assets/imgs/circle_on.svg";
+  static String circleOff = "lib/assets/imgs/circle_off.svg";
 }
