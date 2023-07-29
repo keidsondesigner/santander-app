a5e839379d8f72d302080940a407ffc4b6cf5e99
keidsondesigner <keidsondesigner@gmail.com>
2023-07-28 23:46:02 -0400

    component appBar

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:santander_app/assets/services/user_list.dart';
 import 'package:santander_app/shared/app_colors.dart';
+import 'package:santander_app/shared/app_images.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -23,8 +25,17 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: const Drawer(), //button hamburger top bar
       appBar: AppBar(
         backgroundColor: AppColors.red,
+        foregroundColor: Colors.white, //color white button hamburger top bar
+        title: Center(child: SvgPicture.asset(AppImages.logoSantander, height: 24, width: 24)),
+        actions: [
+          Container(
+            margin: const EdgeInsets.only(right: 16), 
+            child: SvgPicture.asset(AppImages.notification, height: 24)
+          )
+        ],
       ),
     );
   }
