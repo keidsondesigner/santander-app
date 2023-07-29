7cde34550c4891eed067fd04bab70660c4b59757
keidsondesigner <keidsondesigner@gmail.com>
2023-07-28 21:15:45 -0400

    adicionado variaveis de imgs e colors

---
+++ lib/shared/app_images.dart
@@ -0,0 +1,11 @@
+class AppImages {
+  static String logo = "lib/images/logo.svg";
+  static String notification = "lib/images/notification.svg";
+  static String up = "lib/images/up.svg";
+  static String down = "lib/images/down.svg";
+  static String moneyCircle = "lib/images/money_circle.svg";
+  static String handMoney = "lib/images/hand_money.svg";
+  static String screwdriver = "lib/images/screwdriver.svg";
+  static String circleOn = "lib/images/circle_on.svg";
+  static String circleOff = "lib/images/circle_off.svg";
+}
